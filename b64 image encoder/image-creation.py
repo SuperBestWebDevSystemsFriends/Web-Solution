0dcc937114e2421277511443b9904c251a4fd618
cosmomojo <sonicismael2@gmail.com>
2022-09-17 22:10:00 +0930

    Populating content, DB framework written. Currently using BLOBs to represent images in DB. Might be more reliable than file-based approach, but more verbose. Tested with index.php and it works fine.
    Created python tool to convert image to B64 BLOB for database.
    
    Moved styles and scripts into folders.
    Added gitignore rule.

---
+++ b64 image encoder/image-creation.py
@@ -0,0 +1,18 @@
+import base64
+import io
+from PIL import Image
+
+input_file = input("Enter the relative path of the image: ")
+
+with open(input_file, "rb") as image_file:
+    encoded_string = base64.b64encode(image_file.read())
+
+encoded_string = encoded_string.decode('utf-8')
+
+print("Base 64 ended string for image:\n")
+
+print(encoded_string)
+
+output = open("output.txt","w")
+output.write(str(encoded_string))
+output.close()
