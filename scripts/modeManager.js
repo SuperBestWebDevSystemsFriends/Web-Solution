9d03c623c92e91acea700060aa1a6243ad15e830
cosmomojo <sonicismael2@gmail.com>
2022-10-03 13:52:12 +1030

    this is the commit message

--- scripts/modeManager.js
+++ scripts/modeManager.js
@@ -1,21 +1,20 @@
-function init()
-{
-    document.getElementById("lightDarkToggle").addEventListener("click", toggleMode);
-}
-
 function toggleMode(event){
+    const r = document.querySelector(':root');
     console.log("toggleMode");
     if(state="light"){
         state="dark";
-        document.body.style.backgroundColor = "black";
-        document.body.style.color = "white";
+        r.style.setProperty('--clr-light', 'dimgrey');
+        r.style.setProperty('--clr-grey', 'grey');
+        r.style.setProperty('--clr-dark', 'white');
+        r.style.setProperty('--clr-accent-dark', '#b3d7f5');
+        r.style.setProperty('--clr-accent-light', '#004b87');
     }
     if(state="dark"){
         state="light";
-        document.body.style.backgroundColor = "white";
-        document.body.style.color = "black";
+        r.style.setProperty('--clr-light', '#ffffff');
+        r.style.setProperty('--clr-grey', '#cccccc');
+        r.style.setProperty('--clr-dark', '#000000');
+        r.style.setProperty('--clr-accent-dark', '#004b87');
+        r.style.setProperty('--clr-accent-light', '#b3d7f5');
     }
 }
-
-state = "light";
-init();
--- searchresults.php
+++ searchresults.php
@@ -32,10 +32,21 @@
                     exit();
                 }
 
-            $search = $_POST['search']."*";
-            $search_query = $link->prepare("SELECT name FROM Item WHERE MATCH(name) AGAINST (? IN BOOLEAN MODE)");
-            
+                $search = $_POST['searchcategory']."*";
 
+                $search_query = $link->prepare("SELECT name FROM Item WHERE MATCH(name) AGAINST (? IN BOOLEAN MODE)");
+                $search_query->bind_param('s', $search);
+                $search_query->execute();
+                $search_query->store_result();
+                $search_rows = $search_query->num_rows;
+                $search_query->bind_result($product_name);
+
+                if($search_rows > 0){
+                    while($search_query->fetch()){
+                    echo "Your search returned $search_rows results";
+                    echo "$product_name <br>";
+                    }
+                } else { echo "Your search returned no results, sorry :("; }
             ?>
         </div>
 
