7064e2918bc18daa86fb25295677ebc81cebb0e8
R4Sleep <matthew.Sleep@hotmail.com>
2022-10-05 17:09:30 +1030

    added onclick

--- Checkout.php
+++ Checkout.php
@@ -63,7 +63,7 @@
                         echo "<p> Your cart is empty. </p>";
                     }
                     else{
-                        echo "<input type=\"button\" class = \"button2\" id=\"placeOrder\" value=\"Place Order for $". mysqli_fetch_assoc($result)["Total"] ."\">";
+                        echo "<input type=\"button\" onlick=\"hidePayment()\" class = \"button2\" id=\"placeOrder\" value=\"Place Order for $". mysqli_fetch_assoc($result)["Total"] ."\">";
                     }
                     mysqli_free_result($result);
                 }
